ac20c0eba599e00222df54b56e3b5c0cd1dc3af4
LuoTianyi-arm64 <2153585992@qq.com>
2026-08-11 14:25:51 +0800

    feat: 完善echo功能

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use bf_asm::*;
 
 fn main() {
     let mut bf_code = String::new();
-    bf_code.push_str(&echo("Chen Shuoyu hao sao"));
+    bf_code.push_str(&echo("_KOSHINO_ is a pig."));
     simplify_bf!(code bf_code, target bf_code);
     println!("{bf_code}");
 }
