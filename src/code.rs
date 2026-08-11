2bd0f6cdea68fd99db7ed8747a0156439930fcc5
LuoTianyi-arm64 <2153585992@qq.com>
2026-08-11 08:01:43 +0800

    feat: 添加开源许可描述

--- src/code.rs
+++ src/code.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 LuoTianyi-arm64
+
 use std::cell::RefCell;
 use bf_asm::*;
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026 LuoTianyi-arm64
+
 pub mod code;
 pub use code::*;
